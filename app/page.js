61d7818a977b859976015e18bd788571f7fdfe2a
Neil <neilrowland56@yahoo.com>
2023-03-06 12:12:24 +0000

    Installed Tailwind and Prisma

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="container mx-auto  text-center">
+      <h1 className="pt-24 text-5xl font-bold text-teal-700">
+        Home Page - everyone can see this page
+      </h1>
+    </section>
+  );
+}
